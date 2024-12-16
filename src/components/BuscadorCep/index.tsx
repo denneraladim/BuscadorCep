3540b526c017586a2fd0b4e53a9062b1b9469a17
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-12-16 17:09:37 -0300

    Atualizado

--- src/components/BuscadorCep/index.tsx
+++ src/components/BuscadorCep/index.tsx
@@ -1,31 +1,40 @@
-import {Container, Title, ContainerInput, Input, ButtonSearch,  IconSearch, Main, Cep, Address } from './styled';
+import {
+    Container,
+    Title,
+    ContainerInput,
+    Input,
+    ButtonSearch,
+    IconSearch,
+    Main, Cep,
+    Address,
+} from './styled';
 
 
-export default function BuscadorCep(){
+export default function BuscadorCep() {
     return (
         <Container>
-         <Title>Buscador Cep</Title>
+            <Title>Buscador Cep</Title>
 
-        <ContainerInput>
-            <Input
-             type="text"
-             placeholder="Digite seu cep..." 
-            />
+            <ContainerInput>
+                <Input
+                    type="text"
+                    placeholder="Digite seu cep..."
+                />
 
-            <ButtonSearch>
-            <IconSearch/>
-            </ButtonSearch>
-        </ContainerInput>
+                <ButtonSearch>
+                    <IconSearch />
+                </ButtonSearch>
+            </ContainerInput>
 
-        <Main>
-            <Cep>Cep: 79003222</Cep>
+            <Main>
+                <Cep>Cep: 79003222</Cep>
 
-            <Address>Rua teste algum</Address>
-            <Address>Complemento: Algum complemento</Address>
-            <Address>Vila Rosa</Address>
-            <Address>Campo Grande - MS</Address>
-        </Main>
+                <Address>Rua teste algum</Address>
+                <Address>Complemento: Algum complemento</Address>
+                <Address>Vila Rosa</Address>
+                <Address>Campo Grande - MS</Address>
+            </Main>
         </Container>
-        
+
     )
 }
